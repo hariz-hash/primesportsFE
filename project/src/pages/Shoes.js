7b8b87ef0aa600b974eeb11ef8d1a1a641df77df
hariz <hariznyp@gmail.com>
2023-05-15 14:06:37 +0000

    css layout for product page

--- project/src/pages/Shoes.js
+++ project/src/pages/Shoes.js
@@ -1,102 +1,409 @@
 import { useContext, useEffect, useState } from "react"
 import ShoeContext from "../contexts/ShoeContext"
-import { Offcanvas, Button, Form } from 'react-bootstrap';
+import { Offcanvas, Button, Form, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function Shoes() {
 
-  const shoeContext = useContext(ShoeContext);
-  const shoe = shoeContext.getAllProducts();
-  const [searchOptions, setSearchOptions] = useState({})
-  const [search,setSearch] = useState({})
-  const [canvasSearch, setCanvasSearch] = useState(false)
-  const [formFields, setFormFields] = useState(
-    {
-      model: '',
-      brand_id: 0
+  const [show, setShow] = useState(false);
 
-    }
-  )
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-  const updateFormFields = (event) => {
-    setFormFields({
-      ...formFields,
-      [event.target.name]: event.target.value
-    });
+  // const shoeContext = useContext(ShoeContext);
+  // const shoe = shoeContext.getAllProducts() || [];
 
-  };
+  // const [searchOptions, setSearchOptions] = useState({})
 
-  // useEffect(() =>{
-  //     (async () =>{
-  //         await shoeContext.getShoeSearch(search)
-  //     })();
-  // },[search])
-  useEffect(() => {
-    (async () => {
-      const searchOptions = await shoeContext.getSearchOptions();
-      await setSearchOptions(searchOptions);
-      console.log(searchOptions + "adw")
-    })();
-  }, []);
+  // const [search, setSearch] = useState({})
+  // const [formFields, setFormFields] = useState(
+  //   {
+  //     name: '',
+  //     brand_id: '0',
+  //     color_id: '0',
+  //     size_id: '0',
+  //     gender_id: '0',
 
-  const searchShoes = () => {
-    console.log(formFields)
-    const query = { ...formFields };
-    setSearch(query)
-  }
+
+  //   }
+  // )
+  // console.log(formFields + "form fields")
+  // const updateFormFields = (event) => {
+  //   setFormFields({
+  //     ...formFields,
+  //     [event.target.name]: event.target.value
+  //   });
+
+  // };
+
+  // useEffect(() => {
+  //   (async () => {
+  //     await shoeContext.getShoeSearch(search)
+  //   })();
+  // }, [search])
+
+  // useEffect(() => {
+  //   (() => {
+  //     const searchOptions = shoeContext.getOptionsList()
+  //     setSearchOptions(searchOptions);
+  //     console.log(searchOptions)
+  //   })();
+  // }, []);
+
+
+
+  // const searchShoes = () => {
+  //   console.log(formFields)
+  //   const query = { ...formFields };
+  //   setSearch(query)
+  // }
 
   // const generateSelectOptions = (choices) => {
-	// 	if (choices) {
-	// 		return choices.map((choice, index) => {
-	// 			return (
-	// 				<option key={index} value={choice[0]}>
-	// 					{choice[1]}
-	// 				</option>
-	// 			);
-	// 		});
-	// 	}
-	// };
+  //   if (choices) {
+  //     return choices.map((choice, index) => {
+  //       return (
+  //         <option key={index} value={choice[0]}>
+  //           {choice[1]}
+  //         </option>
+  //       );
+  //     });
+  //   }
+  // };
 
-  const displayCanvas = () => setCanvasSearch(true)
-  const closeCanvas = () => setCanvasSearch(false)
+
+
 
   return <>
 
     <h1> Display Shoes+ search form </h1>
-    <Button variant="primary" onClick={displayCanvas}>
-      Launch
-    </Button>
 
-    <Offcanvas show={canvasSearch} onHide={closeCanvas}>
-      <Offcanvas.Header closeButton>
-        <Offcanvas.Title>search</Offcanvas.Title>
-      </Offcanvas.Header>
-      <Offcanvas.Body>
-        <Form>
-          <Form.Group>
-            <Form.Label>
-              Brand
-            </Form.Label>
-            <Form.Control
-              type="text"
-              name="model"
-              value={formFields.model}
-              onChange={updateFormFields}>
+    <div className="container-fluid mt-4">
+      <div className="row">
+        <div className="col-md-3 d-none d-md-block  ">
+          <div className="form-group row">
+            <div className="col-xs-2">
+              <label htmlFor="ex1">Brands</label>
+              <input className="form-control" id="ex1" type="text" />
+            </div>
+            <div className="col-xs-2">
+              <label htmlFor="ex1">Type</label>
+              <input className="form-control" id="ex1" type="text" />
+            </div>
+          </div>
+          <div className="col-xs-2 my-4">
+            <div className="row">
+              <div className="col" >
+                <select
+                  className="form-select"
+                  aria-label="Default select example"
+                >
+                  <option >Select Materials</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div className="col">
+                <select
+                  className="form-select"
+                  aria-label="Default select example"
+                >
+                  <option >Select Colors</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-2 my-4">
+            <div className="row">
+              <div className="col" >
+                <select
+                  className="form-select"
+                  aria-label="Default select example"
+                >
+                  <option >Select Size</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div className="col">
+                <select
+                  className="form-select"
+                  aria-label="Default select example"
+                >
+                  <option >Select Genders</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-2 ">
+            <div className="row">
+              <div className="col" >
+                <label htmlFor="ex3">Min Cost</label>
+                <input className="form-control" id="ex3" type="text" />
+              </div>
+              <div className="col">
+                <label htmlFor="ex3">Max Cost</label>
+                <input className="form-control" id="ex3" type="text" />
+              </div>
+            </div>
+          </div>
+        </div>
 
-            </Form.Control>
-          </Form.Group>
-          {/* <Form.Group>
-            <Form.Select aria-label="Brand"
-            name="brand_id"
-            value={formFields.brand_id}
-            onChange={updateFormFields}>
-           {generateSelectOptions(
-												searchOptions.brand
-											)}
-            </Form.Select>
-          </Form.Group> */}
 
-        </Form>
-      </Offcanvas.Body>
-    </Offcanvas>
+        <Button variant="primary" onClick={handleShow} className="col-md-3 d-lg-none d-md-none">
+          Launch
+        </Button>
+
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <div className="form-group row">
+              <div className="col-xs-2">
+                <label htmlFor="ex1">Brands</label>
+                <input className="form-control" id="ex1" type="text" />
+              </div>
+              <div className="col-xs-2">
+                <label htmlFor="ex1">Type</label>
+                <input className="form-control" id="ex1" type="text" />
+              </div>
+            </div>
+            <div className="col-xs-2 my-4">
+              <div className="row">
+                <div className="col" >
+                  <select
+                    className="form-select"
+                    aria-label="Default select example"
+                  >
+                    <option >Select Materials</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                </div>
+                <div className="col">
+                  <select
+                    className="form-select"
+                    aria-label="Default select example"
+                  >
+                    <option >Select Colors</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="col-xs-2 my-4">
+              <div className="row">
+                <div className="col" >
+                  <select
+                    className="form-select"
+                    aria-label="Default select example"
+                  >
+                    <option >Select Size</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                </div>
+                <div className="col">
+                  <select
+                    className="form-select"
+                    aria-label="Default select example"
+                  >
+                    <option >Select Genders</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="col-xs-2 ">
+              <div className="row">
+                <div className="col" >
+                  <label htmlFor="ex3">Min Cost</label>
+                  <input className="form-control" id="ex3" type="text" />
+                </div>
+                <div className="col">
+                  <label htmlFor="ex3">Max Cost</label>
+                  <input className="form-control" id="ex3" type="text" />
+                </div>
+              </div>
+            </div>
+          </Offcanvas.Body>
+        </Offcanvas>
+
+
+        <div className="col-md-9 ">
+            <div className="row  mt-4">
+              <div className="col-md-4 mb-3  ">
+                <div className="card">
+                  {/* <img src="..." className="card-img-top" alt="..." /> */}
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 mb-3 ">
+                <div className="card">
+                  {/* <img src="..." className="card-img-top" alt="..." /> */}
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 mb-3  ">
+                <div className="card">
+                  {/* <img src="..." className="card-img-top" alt="..." /> */}
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 mb-3 ">
+                <div className="card">
+                  {/* <img src="..." className="card-img-top" alt="..." /> */}
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 mb-3  ">
+                <div className="card">
+                  {/* <img src="..." className="card-img-top" alt="..." /> */}
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 mb-3 ">
+                <div className="card">
+                  {/* <img src="..." className="card-img-top" alt="..." /> */}
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 mb-3  ">
+                <div className="card">
+                  {/* <img src="..." className="card-img-top" alt="..." /> */}
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 mb-3 ">
+                <div className="card">
+                  {/* <img src="..." className="card-img-top" alt="..." /> */}
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+           
+         
+           
+           
+
+            </div>
+            
+        </div>
+        
+
+
+      </div>
+    </div>
+
+
+    {/* <div className="container-fluid background">
+      <h1 className='m-4'>Search a shoe</h1>
+      <Form.Group className='m-4 titleFont'>
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Search by name" name="name" value={formFields.name} onChange={updateFormFields}>
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group className='m-4 titleFont'>
+
+        <Button className="btn mt-3 ml-3" onClick={searchShoes}>Search</Button>
+      </Form.Group>
+      <Form.Group className='mb-3'>
+        <Form.Label>
+          Brand
+        </Form.Label>
+        <Form.Select name='brand_id' value={formFields.brand_id} onChange={updateFormFields}>
+
+          {generateSelectOptions(searchOptions.brands)}
+
+        </Form.Select>
+        <Form.Select name='color_id' value={formFields.color_id} onChange={updateFormFields}>
+
+          {generateSelectOptions(searchOptions.colors)}
+
+        </Form.Select>
+        <Form.Select name='size_id' value={formFields.size_id} onChange={updateFormFields}>
+
+          {generateSelectOptions(searchOptions.sizes)}
+
+        </Form.Select>
+        <Form.Select name='materials_id' value={formFields.materials_id} onChange={updateFormFields}>
+
+          {generateSelectOptions(searchOptions.materials)}
+
+        </Form.Select>
+      </Form.Group>
+    </div>
+
+
+    <div className="tabs">
+      <div className="container-fluid">
+        <h1 className='mx-4 my-2 '>Choose a shoe</h1>
+
+        {shoe.length ? (
+          <div class="row row-cols-md-3">{
+            shoe.map((shoe) => {
+              return (
+
+                <div class="col gy-3" key={shoe.id}>
+                  <li>{shoe.name}</li>
+                </div>
+              );
+            })}</div>
+        ) : <div>
+          <h5>No shoes found</h5>
+        </div>}
+      </div>
+
+    </div> */}
+
+
+
   </>
 }
--- project/src/providers/ShoeProvider.js
+++ project/src/providers/ShoeProvider.js
@@ -1,41 +1,81 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ShoeContext from '../contexts/ShoeContext';
 import axios from 'axios';
 
-const BASE_URL = "https://3000-harizhash-primesports-lun7xzofp6v.ws-us96b.gitpod.io";
+const BASE_URL = "https://3000-harizhash-primesports-lun7xzofp6v.ws-us97.gitpod.io/api";
 
-export default function ShoeProvider(props)
-{
+export default function ShoeProvider(props) {
 
-    const [products, setProducts] =useState([])
-    
-    const shoeContext ={
-        getAllProducts: () =>
-        {
+
+    let brands = [
+        [1, "Adidas"], [2, "Nike"], [3, "Puma"],
+        [4, "Reebok"], [5, "New Balance"],
+        [6, "Under Armour"]
+    ]
+    let genders = [
+        [1, "Male"],
+        [2, "Female"],
+    ];
+    let materials = [
+        [1, "Carbon fibre"], [2, "Polyamide"],
+        [3, "Polyester"], [4, "Rubber"]
+    ]
+    let sizes = [
+        [1, "5.5"], [2, "6"], [3, "6.5"],
+        [4, "7"], [5, "7.5"], [6, "8"],
+        [7, "8.5"], [8, "9"], [9, "9.5"],
+        [10, "10"], [11, "10.5"], [12, "11"],
+        [13, "11.5"], [14, "12"]
+    ]
+    let colors = [
+        [1, "Red"], [2, "Yellow"], [3, "Orange"],
+        [4, "Pink"], [5, "Green"], [6, "Blue"],
+        [7, "White"], [8, "Silver"], [9, "Grey"], [10, "Black"]
+    ]
+
+    brands.unshift([0, '--- Any Brand ---']);
+    materials.unshift([0, '--- Any Materials ---']);
+    sizes.unshift([0, '--- Any Sizes ---']);
+    colors.unshift([0, '--- Any Colors ---']);
+
+    const options = {
+        brands,
+        materials,
+        sizes,
+        colors,
+        genders
+    }
+
+    const [products, setProducts] = useState([])
+    const [optionList, setOptionList] = useState({})
+
+    const shoeContext = {
+        getAllProducts: () => {
             return products;
         },
-        getShoeSearch: async(query) =>
-        {
-            const response = await axios.get(BASE_URL + '/product/search',{
-                params:query
+        getShoeSearch: async (query) => {
+            const response = await axios.get(BASE_URL + '/product/search', {
+                params: query
             })
-            console.log("Search data " + response.data)
-            const searchResult = response.data
-            setProducts(response)
+            console.log("Search data " + response.data.searchShoes)
+            const searchResult = response.data.searchShoes
+            setProducts(searchResult)
             return searchResult
         },
-        getSearchOptions: async () =>
-        {
+        getSearchOptions: async () => {
             const response = await axios.get(BASE_URL + '/product/options')
-            const options = response.data.options;
-            console.log(options)
+            const searchOptions = response.data.options;
+            console.log("de")
+            return searchOptions;
+        },
+        getOptionsList: () => {
             return options;
         }
 
 
     }
 
-    return(
+    return (
         <ShoeContext.Provider value={shoeContext}>
             {props.children}
         </ShoeContext.Provider>
